0cc6e0b1b2d2f799ad5fbd1427b08d5f28073a9c
ariansevillano <ariansevillano1@gmail.com>
2025-02-18 23:06:18 -0500

    Prueba del sistema funcional

--- src/main/java/packagesistemadeventas/prueba/PruebaVentas.java
+++ src/main/java/packagesistemadeventas/prueba/PruebaVentas.java
@@ -1,13 +1,25 @@
 package packagesistemadeventas.prueba;
 
+import packagesistemadeventas.ventas.Orden;
 import packagesistemadeventas.ventas.Producto;
 
 public class PruebaVentas {
     public static void main(String[] args) {
         System.out.println("***Sistema de Ventas***");
         Producto producto1 = new Producto("1 kg Arroz", 3.80);
-        System.out.println(producto1);
         Producto producto2 = new Producto("1 kg Lentejas", 4.40);
-        System.out.println(producto2);
+
+
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+
+        Orden orden2 = new Orden();
+        //También podríamos primero crear la variable, pero así también se puede
+        orden2.agregarProducto(new Producto("1 kg Chuleta de cerdo",45.50));
+        //la única diferencia es que ese objeto creado ya no puede ser reutilizado
+        orden2.agregarProducto(producto2);
+        orden2.mostrarOrden();
     }
 }
