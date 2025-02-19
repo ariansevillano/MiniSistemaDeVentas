21e55bb584014de465d9ab1f76de88b6d581f355
ariansevillano <ariansevillano1@gmail.com>
2025-02-18 22:29:09 -0500

    Creación clase Orden y método agregarProducto

--- src/main/java/packagesistemadeventas/ventas/Orden.java
+++ src/main/java/packagesistemadeventas/ventas/Orden.java
@@ -1,4 +1,23 @@
 package packagesistemadeventas.ventas;
 
 public class Orden {
+    private final int idOrden;
+    private Producto[] productos;
+    private int contadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+    private static int contadorOrdenes;
+
+
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto){
+        if (this.contadorProductos < Orden.MAX_PRODUCTOS)
+            this.productos[this.contadorProductos++] = producto;
+        else
+            System.out.println("Se superó el máximo de productos: "
+            + Orden.MAX_PRODUCTOS);
+    }
 }
